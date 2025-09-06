333fc34b63e719afb46bafa570e39310364e632a
Mathvivas <mathvivas@hotmail.com>
2025-09-06 14:47:55 -0300

    Criação do arquivo app e da classe modelo Book

---
+++ books-api-tech-challenge/app/models/book.py
@@ -0,0 +1,14 @@
+from . import db
+
+class Book(db.Model):
+    __tablename__ = 'books'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    availability = db.Column(db.Integer, nullable=False)
+    category = db.Column(db.String(80), nullable=False)
+    image = db.Column(db.String(90), nullable=False)
+
+    def __repr__(self):
+        return f'<Book {self.title}>'

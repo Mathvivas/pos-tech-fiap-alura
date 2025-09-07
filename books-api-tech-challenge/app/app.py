8c59f34299c1e2832ebc771e3ed14a33ab5df6cd
Mathvivas <mathvivas@hotmail.com>
2025-09-07 16:16:27 -0300

    Modificação da criação do DB, para que os dados sejam importados e o banco crie um id automático para cada dado

--- books-api-tech-challenge/app/app.py
+++ books-api-tech-challenge/app/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from models import db
 import pandas as pd
 from sqlalchemy import create_engine
+from models import Book
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -12,9 +13,13 @@ def import_csv_to_db():
     with app.app_context():
         db.create_all()
 
+        if Book.query.first():
+            print('Database already contains books. Skipping CSV import.')
+            return
+
         df = pd.read_csv(app.config['CSV_FILE_PATH'])
         engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-        df.to_sql('books', engine, if_exists='replace', index=False)
+        df.to_sql('books', engine, if_exists='append', index=False)
 
 from routes import *
 

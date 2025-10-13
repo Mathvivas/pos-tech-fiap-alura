04f8a0f68dd7fb2d1eede73a5fd11408b40694bf
Mathvivas <mathvivas@hotmail.com>
2025-10-13 15:02:20 -0300

    Mudança na posição dos elementos

--- books-api-tech-challenge/app/streamlit_app.py
+++ books-api-tech-challenge/app/streamlit_app.py
@@ -1,14 +1,8 @@
 import requests
 import streamlit as st
-from app import app, logger
 import pandas as pd
 import datetime
-import threading
-import nltk
-
-
-app.config.from_object('config')
-app.json.ensure_ascii = False
+import config
 
 @st.cache_resource
 def load_data(file_path):
@@ -28,6 +22,100 @@ st.set_page_config(
     page_title='Books API',
     page_icon=':orange_book:'
 )
+
+@st.cache_resource
+def setup_nltk():
+    import nltk
+    nltk.download('stopwords')
+
+setup_nltk()
+    
+if 'df' not in st.session_state:
+    df = load_data(config.CSV_FILE_PATH)
+    st.session_state['df'] = df
+
+if 'embeddings' not in st.session_state:
+    df_embeddings = load_data(config.CSV_FILE_PATH_EMBEDDINGS)
+    st.session_state['embeddings'] = df_embeddings
+
+if 'metric' not in st.session_state:
+    st.session_state['metric'] = 0
+    metric = st.session_state['metric']
+
+if 'history' not in st.session_state:
+    st.session_state['history'] = pd.DataFrame(columns=['Time', 'Metric'])
+
+def sidebar():
+
+    st.html("""
+        <style>
+            [alt=Logo] {
+                padding-top: 1rem;
+                height: 10rem;
+            }
+        </style>
+    """)
+    
+    with st.sidebar:
+        st.logo('../images/book-logo-nbg.png')
+        st.title('API de Livros')
+        st.markdown('-----')
+        
+        with st.expander('Usuário', icon=':material/account_box:'):
+            user = st.text_input('Usuário')
+            password = st.text_input('Senha', type='password')
+            col1, col2 = st.columns(2)
+            
+            with col1:
+                register = st.button('Registrar', icon=':material/person_add:')
+            
+            with col2:
+                login = st.button('Login', icon=':material/login:')
+            
+            if register:
+                auth = {
+                    'username': user,
+                    'password': password
+                }
+                response_register = requests.post('http://localhost:5000/api/v1/auth/register', json=auth)
+            
+                if response_register.status_code == 201:
+                    st.success(response_register.text)
+                else:
+                    st.error(response_register.text)
+            if login:
+                auth = {
+                    'username': user,
+                    'password': password
+                }
+            
+                response_login = requests.post('http://localhost:5000/api/v1/auth/login', json=auth)
+            
+                if response_login.status_code == 201:
+                    dados_json = response_login.json()
+                    st.success('Token: ' + dados_json.get('access_token'))
+                else:
+                    st.error(response_login.text)
+
+        with st.expander('Token', icon=':material/token:'):
+            token = st.text_input('Cole o token aqui:', type='password')
+            st.session_state['token'] = token
+            atualizar = st.button('Renovar token prestes a expirar')
+            
+            if atualizar:
+                header = {'Authorization': f'Bearer {token}'}
+                response_atualizar = requests.post('http://localhost:5000/api/v1/auth/refresh', headers=header)
+
+                if response_atualizar.status_code == 200:
+                    dados_json = response_atualizar.json()
+                    st.success(dados_json.get('access_token'))
+                else:
+                    st.error([response_atualizar.status_code, response_atualizar.content])
+
+        st.markdown('-----')
+        st.page_link("http://localhost:5000/apidocs/", label="Documentação", icon=':material/docs:')
+
+sidebar()
 
 pages = {
     'Web Scraping': [
@@ -52,93 +140,6 @@ pages = {
         st.Page('pages/machine_learning.py', title='Machine Learning', icon=':material/robot_2:')
     ]
 }
-
-nltk.download('stopwords')
-    
-if 'df' not in st.session_state:
-    df = load_data(app.config['CSV_FILE_PATH'])
-    st.session_state['df'] = df
-
-if 'embeddings' not in st.session_state:
-    df_embeddings = load_data(app.config['CSV_FILE_PATH_EMBEDDINGS'])
-    st.session_state['embeddings'] = df_embeddings
-
-logger.info(f'start of streamlit_test, {threading.get_ident()}')
-
-if 'metric' not in st.session_state:
-    st.session_state['metric'] = 0
-    metric = st.session_state['metric']
-
-if 'history' not in st.session_state:
-    st.session_state['history'] = pd.DataFrame(columns=['Time', 'Metric'])
-
-st.html("""
-    <style>
-        [alt=Logo] {
-            padding-top: 1rem;
-            height: 10rem;
-        }
-    </style>
-""")
-
-with st.sidebar:
-    st.logo('../images/book-logo-nbg.png')
-    st.title('API de Livros')
-    st.markdown('-----')
-    
-    with st.expander('Usuário', icon=':material/account_box:'):
-        user = st.text_input('Usuário')
-        password = st.text_input('Senha', type='password')
-        col1, col2 = st.columns(2)
-        
-        with col1:
-            register = st.button('Registrar', icon=':material/person_add:')
-        
-        with col2:
-            login = st.button('Login', icon=':material/login:')
-        
-        if register:
-            auth = {
-                'username': user,
-                'password': password
-            }
-            response_register = requests.post('http://localhost:5000/api/v1/auth/register', json=auth)
-        
-            if response_register.status_code == 201:
-                st.success(response_register.text)
-            else:
-                st.error(response_register.text)
-        if login:
-            auth = {
-                'username': user,
-                'password': password
-            }
-        
-            response_login = requests.post('http://localhost:5000/api/v1/auth/login', json=auth)
-        
-            if response_login.status_code == 201:
-                dados_json = response_login.json()
-                st.success('Token: ' + dados_json.get('access_token'))
-            else:
-                st.error(response_login.text)
-
-    with st.expander('Token', icon=':material/token:'):
-        token = st.text_input('Cole o token aqui:', type='password')
-        st.session_state['token'] = token
-        atualizar = st.button('Renovar token prestes a expirar')
-        
-        if atualizar:
-            header = {'Authorization': f'Bearer {token}'}
-            response_atualizar = requests.post('http://localhost:5000/api/v1/auth/refresh', headers=header)
-
-            if response_atualizar.status_code == 200:
-                dados_json = response_atualizar.json()
-                st.success(dados_json.get('access_token'))
-            else:
-                st.error([response_atualizar.status_code, response_atualizar.content])
-
-    st.markdown('-----')
-    st.page_link("http://localhost:5000/apidocs/", label="Documentação", icon=':material/docs:')
 
 footer="""<style>
 a:link , a:visited{

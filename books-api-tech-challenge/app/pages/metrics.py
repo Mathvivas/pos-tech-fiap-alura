e0435065f6da92e588de9944e2f9a46650fbfd06
Mathvivas <mathvivas@hotmail.com>
2025-10-17 12:06:19 -0300

    Criação do gitignore pelo gitignore.io e adição de checagem no gráfico de métricas

--- books-api-tech-challenge/app/pages/metrics.py
+++ books-api-tech-challenge/app/pages/metrics.py
@@ -6,4 +6,5 @@ app.config.from_object('config')
 app.json.ensure_ascii = False
 
 st.metric(label='Rotas chamadas com sucesso', value=st.session_state['metric'], border=True)
-st.line_chart(st.session_state['history'], x='Time', y='Metric', x_label='Hora', y_label='Chamadas')
+if not st.session_state['history'].empty:
+    st.line_chart(st.session_state['history'], x='Time', y='Metric', x_label='Hora', y_label='Chamadas')

a63a47ee9deaeec04c1261883e4880123bbd5567
Mathvivas <mathvivas@hotmail.com>
2025-10-21 10:56:58 -0300

    Criação do DAG

--- books-api-tech-challenge/app/__init__.py
+++ books-api-tech-challenge/app/__init__.py
@@ -3,3 +3,4 @@ from .app import app
 from .app import run
 from .streamlit_app import setar_metrica, split_frame
 from .data_cleaning import data_cleaning, split_data, vectorize, find_similar
+from .pages import web_scraping
